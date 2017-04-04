534c920f1999f9f543b93c089ce85e447227e3a1
Jean-Philippe DEMORY <jean-philippe.demory@u-picardie.fr>
2017-04-04 14:25:34 +0200

    Ajout du projet WPF en VB

---
+++ Ihm Wpf Vb/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Imports Bibliotheque
+
+Class MainWindow
+    Private Sub btnOK_Click(sender As Object, e As RoutedEventArgs)
+        Dim hw As New HelloWorld(tbPrenom.Text)
+
+        lblMsg.Content = hw.Bonjour()
+    End Sub
+End Class
