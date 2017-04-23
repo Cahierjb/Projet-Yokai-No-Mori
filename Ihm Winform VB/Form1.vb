fcf5e197297d80bc019a5e1de8aa747899846d6f
Jean-Philippe DEMORY <jean-philippe.demory@u-picardie.fr>
2017-04-23 09:00:42 +0200

    Ajout du projet WinForm en VB

---
+++ Ihm Winform VB/Form1.vb
@@ -0,0 +1,9 @@
+﻿Imports Bibliotheque
+
+Public Class Form1
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        Dim hw As New HelloWorld(tbPrenom.Text)
+
+        lblMessage.Text = hw.Bonjour()
+    End Sub
+End Class
