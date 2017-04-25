bcece6e9f2d3ea5d291dadece3960cd7eff87e1f
Carlier Adrien <adrien.carlier@etud.u-picardie.fr>
2017-04-25 15:59:47 +0200

    Ajout des champs nom et position dans la classe Piece

--- Bibliotheque/Pieces.cs
+++ Bibliotheque/Pieces.cs
@@ -7,5 +7,29 @@ namespace Bibliotheque
 {
     public class Pieces
     {
+        // Champs
+        private string nom;
+        private int[,] position;
+
+
+        // Constructeur
+        public Pieces(string _nom, int[,] _pos)
+        {
+            Nom = _nom;
+            Position = _pos;
+        }
+
+        // Propriétés
+        public string Nom { get; set; }
+        public int[,] Position { get; set; }
+
+
+
+        // Méthode
+
+        public int[,] Deplace()
+        {
+            
+        }
     }
 }
